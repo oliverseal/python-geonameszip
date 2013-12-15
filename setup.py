6018abf81b26834cff2c61d03ef26aa05b624eda
Oliver Wilkerson <oliver.wilkerson@gmail.com>
2013-12-15 10:15:35 -0600

    moved __init__.py to the root for easier package install

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+setup(
+    name='geonameszip',
+    version='0.1.0',
+    description='Quick and dirty script/api for syncing postal codes / zip codes with a local sqlite3 database.',
+    long_description=(open('README.md').read() + '\n\n' +
+                      open('LICENSE.md').read()),
+    url='https://github.com/oliverseal/python-geonameszip',
+    license='MIT',
+    author='Oliver Wilkerson',
+    author_email='oliver.wilkerson@gmail.com',
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'License :: Other/Proprietary License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
